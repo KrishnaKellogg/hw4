dec05e470767e9673f7fe48c768040166e3f67eb
Krishna Surakanti <krishna.surakanti@kellogg.northwestern.edu>
2023-05-31 13:14:24 +0000

    edited all controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome! You've successfully logged in."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "You've entered an incorrect name or password."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "You need to create a new account."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye! You're now logged out."
+    redirect_to "/login"
   end
 end
   
